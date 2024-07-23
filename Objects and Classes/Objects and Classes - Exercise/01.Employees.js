7bfa9b03360f86115190e5917fa49e2643f0bf31
Rosen Roydev <rosen.roydev@gmail.com>
2024-07-23 16:59:53 +0300

    1

---
+++ Objects and Classes/Objects and Classes - Exercise/01.Employees.js
@@ -0,0 +1,19 @@
+function employees(input){
+    let namesWithLength = {};
+    for(let name of input) 
+        {
+            let length = name.length
+         namesWithLength[name] = length;
+        }
+        let entries = Object.entries(namesWithLength)
+        for (let [name,length] of entries) {
+            console.log(`Name: ${name} -- Personal Number: ${length}`)
+        }
+}
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    )

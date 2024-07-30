5047fcd6b9972bd5c51be16c5f0296d698f116b5
Rosen Roydev <rosen.roydev@gmail.com>
2024-07-30 13:54:43 +0300

    3

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -1,4 +1,8 @@
 function calc() {
     // TODO: sum = num1 + num2
-    
+    num1 = document.getElementById('num1');
+    num2 = document.getElementById('num2');
+    sum = document.getElementById('sum')
+    sum.value= Number(num1.value) + Number(num2.value);
+    return sum
 }

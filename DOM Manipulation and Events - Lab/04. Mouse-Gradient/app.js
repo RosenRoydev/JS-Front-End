a074ded8aaae9511a2fc0409eb7291042e5b7443
Rosen Roydev <rosen.roydev@gmail.com>
2024-08-06 22:05:35 +0300

    4-6

--- DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientEl = document.getElementById('gradient');
+    const result = document.getElementById('result');
+    gradientEl.addEventListener('mousemove', (e) =>{
+        const currentPosition = e.offsetX;
+        const totalWidth = e.currentTarget.clientWidth;
+        let percentage = Math.floor((currentPosition/totalWidth) * 100);
+        result.textContent = `${percentage}%`
+    });
+    
 }
--- DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+/;
+    const mailInput = document.getElementById('email');
+    console.log(mailInput)
+    mailInput.addEventListener("change", (e)=>{
+   
+       if(!pattern.test(e.currentTarget.value)){
+          mailInput.classList.add('error');
+       }else{
+        mailInput.classList.remove('error');
+       }
+    })
 }

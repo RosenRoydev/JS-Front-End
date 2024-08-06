0a7d0dc661f7dfd72cce3856c0d446b7f30dbe37
Rosen Roydev <rosen.roydev@gmail.com>
2024-08-06 17:52:00 +0300

    2-3

--- DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const customersTable = document.getElementById('customers');
+    const inputEl = document.querySelector('input[type=text][name=email]');
+    const resultEl = document.getElementById('result');
+
+    let searchMail = inputEl.value;
+    
+    const allMails = document.querySelectorAll('tbody td:last-child');
+   let findMail = (Array.from(allMails).find(m => m.textContent === searchMail));
+   
+    if(findMail){
+        findMail.parentElement.remove();
+        resultEl.textContent = 'Deleted.'
+    }else{
+        resultEl.textContent ='Not found.';
+    }
+    
 }
--- DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    const ulElements = document.getElementById('items');
+    const inputEl = document.getElementById('newItemText');
+
+    let newLi = document.createElement('li');
+    newLi.textContent = inputEl.value;
+    let aElement = document.createElement('a');
+    aElement.textContent = '[Delete]';
+    aElement.href = '#'
+    newLi.append(aElement);
+    
+    aElement.addEventListener('click', (e) =>{
+        e.currentTarget.parentElement.remove()
+    })
+    ulElements.appendChild(newLi)
+    
+    inputEl.value = '';
+
 }

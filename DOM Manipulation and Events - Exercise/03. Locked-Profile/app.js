f618388ad9fc08968db713615f3c36fc1b5555d8
Rosen Roydev <rosen.roydev@gmail.com>
2024-08-09 10:11:30 +0300

    3-4

--- DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profilesEl = document.querySelectorAll('.profile')
+    for (const profile of profilesEl) {
+        const buttonEl = profile.querySelector('button')
+        const radioLockEl = profile.querySelector('input[type=radio][value=lock]')
+        buttonEl.addEventListener('click', () => {
+            if (radioLockEl.checked) {
+                return
+            }
+            const additionalInfo = buttonEl.previousElementSibling;
+            if (buttonEl.textContent === 'Show more') {
+                additionalInfo.style.display = 'block'
+                buttonEl.textContent = 'Hide it'
+            } else {
+                additionalInfo.style.display = 'none'
+                buttonEl.textContent = 'Show more'
+            }
+        })
+    }
 }
--- DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+     const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+    const optionMenuEl = document.getElementById('menu')
+       let newOption =  document.createElement('option');
+       newOption.textContent = newItemText.value;
+       newOption.value = newItemValue.value
+       optionMenuEl.appendChild(newOption);
+       newItemText.value = '';
+    newItemValue.value = ''; 
 }

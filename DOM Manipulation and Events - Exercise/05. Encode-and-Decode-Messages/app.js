de2f94e111deec0a44b65329095d7bd30e336fac
Rosen Roydev <rosen.roydev@gmail.com>
2024-08-09 21:08:42 +0300

    5-6

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,21 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttonEncodeEl = document.querySelector('#main div button');
+    const buttonDecodeEl = document.querySelector('#main div:nth-child(2) button');
+    const textForDecode = buttonDecodeEl.previousElementSibling;
+    buttonEncodeEl.addEventListener('click', ()=>{
+        const textForEncode = buttonEncodeEl.previousElementSibling;
+        let encodeText=''
+        for (const word of textForEncode.value) {
+            encodeText += String.fromCharCode(word.charCodeAt(0) + 1)
+        }
+        textForDecode.value = encodeText;
+        textForEncode.value = '';
+    })
+    buttonDecodeEl.addEventListener('click', ()=>{
+        let decodeText = '';
+        for(const char of textForDecode.value){
+            decodeText += String.fromCharCode(char.charCodeAt(0) - 1)
+        }
+        textForDecode.value = decodeText;
+    })
 }
--- DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,64 @@
 function solve() {
+ const generateButton = document.querySelector('#exercise button');
+ const furnitureDetails = generateButton.previousElementSibling;
+ const table = document.getElementsByClassName('table')[0];
+ const tBody = table.querySelector('table tbody');
+ const buyButton = document.querySelector('button:nth-of-type(2)');
+ const outputArea = document.querySelector('textarea:nth-of-type(2)')
+ 
 
-  //TODO...
+ generateButton.addEventListener('click', ()=>{
+  let furnitures = JSON.parse(furnitureDetails.value);
+  for (const furniture of furnitures) {
+    const imgEl = document.createElement('img');
+    imgEl.src = furniture.img
+    const imageTdEl = document.createElement('td')
+    imageTdEl.appendChild(imgEl);
+    const nameEl = document.createElement('p');
+    nameEl.textContent = furniture.name;
+    const nameTdEl = document.createElement('td');
+    nameTdEl.appendChild(nameEl);
+    const price = document.createElement('p');
+    price.textContent = furniture.price;
+    const priceTdEl = document.createElement('td')
+    priceTdEl.appendChild(price)
+    const decFac = document.createElement('p');
+    decFac.textContent = furniture.decFactor;
+    const decFacTdEl = document.createElement('td');
+    decFacTdEl.appendChild(decFac);
+    const checkEl = document.createElement('input')
+    checkEl.setAttribute('type','checkbox');
+    const checkTdEl = document.createElement('td');
+    checkTdEl.appendChild(checkEl);
+    const newTr = document.createElement('tr');
+    
+    newTr.appendChild(imageTdEl);
+    newTr.appendChild(nameTdEl);
+    newTr.appendChild(priceTdEl);
+    newTr.appendChild(decFacTdEl);
+    newTr.appendChild(checkTdEl);
+    tBody.appendChild(newTr);
+  }
+  
+ })
+ buyButton.addEventListener('click', () =>{
+  let names = [];
+  let totalPrice = 0;
+  let averageDecorationFactor = 0;
+  let counter = 0;
+  Array.from(tBody.children).forEach(trEl =>{
+    let checked = trEl.querySelector('input[type=checkbox]')
+    if(!checked.checked){
+      return
+    }
+    totalPrice += Number(trEl.children[2].textContent)
+    names.push(trEl.children[1].textContent);
+    averageDecorationFactor += Number(trEl.children[3].textContent)
+    counter++;
+  })
+  averageDecorationFactor = averageDecorationFactor/counter;
+ outputArea.textContent +=`Bought furniture: ${names.join(', ')}\n`;
+ outputArea.textContent += `Total price: ${totalPrice.toFixed(2)}\n`;
+ outputArea.textContent += `Average decoration factor: ${averageDecorationFactor}\n`
+ })
 }
